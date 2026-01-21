12eac983488f7b3f7604ec1cbaaffc7950263c58
Rodrigo Almada <rfalmada7@gmail.com>
2026-01-21 00:15:28 +0000

    Added Navigation Bar

---
+++ script.js
@@ -0,0 +1,9 @@
+function showVertbar(){
+	const vertbar = document.querySelector('.vertbar')
+	vertbar.style.display = 'flex'
+}
+
+function hideVertbar(){
+	const vertbar = document.querySelector('.vertbar')
+	vertbar.style.display = 'none'
+}

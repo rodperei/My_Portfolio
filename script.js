e7911b580a812de84c7ef853e3952521af2d20cf
Rodrigo Almada <rfalmada7@gmail.com>
2026-01-24 10:07:55 +0000

    Added footer and call to action to index

--- script.js
+++ script.js
@@ -7,3 +7,9 @@ function hideVertbar(){
 	const vertbar = document.querySelector('.vertbar')
 	vertbar.style.display = 'none'
 }
+
+document.querySelector(".email").addEventListener('click', function(e){
+	e.preventDefault();
+	
+	window.location.href = 'mailto:rfalmada7@gmail.com';
+});
